294f4d6f38114fb221f11d62f43644e429b8699d
Elior Erez <eerez@redhat.com>
2022-09-07 18:01:10 +0300

    Fix script and support PYTHON_PRERELEASE_TAG

--- python_builder/build.sh
+++ python_builder/build.sh
@@ -7,10 +7,17 @@ BASE_IMAGE_OS=${BASE_IMAGE_OS:-quay.io/centos/centos:"${OS_RELEASE_TAG}"}
 PYTHON_VERSION=${PYTHON_VERSION:-"${LATEST_PYTHON_VERSION}"}
 
 
-dj --dockerfile Dockerfile.jinja --outfile Dockerfile \
- --env BASE_IMAGE_OS="${BASE_IMAGE_OS}" \
- --env PYTHON_VERSION="${PYTHON_VERSION}" \
- --env PYTHON_PRERELEASE_TAG="${PYTHON_PRERELEASE_TAG} "
+if [[ -n ${PYTHON_PRERELEASE_TAG} ]]; then
+  dj --dockerfile Dockerfile.jinja --outfile Dockerfile \
+   --env BASE_IMAGE_OS="${BASE_IMAGE_OS}" \
+   --env PYTHON_VERSION="${PYTHON_VERSION}" \
+   --env PYTHON_PRERELEASE_TAG="${PYTHON_PRERELEASE_TAG}"
+else
+  dj --dockerfile Dockerfile.jinja --outfile Dockerfile \
+   --env BASE_IMAGE_OS="${BASE_IMAGE_OS}" \
+   --env PYTHON_VERSION="${PYTHON_VERSION}"
+fi
+
 
 IMAGE=quay.io/eerez/python-builder:"${OS_RELEASE_TAG}-${PYTHON_VERSION}${PYTHON_PRERELEASE_TAG}"
 docker build . -t "${IMAGE}"
